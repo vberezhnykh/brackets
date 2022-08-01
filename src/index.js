e9c39be09d73947e89d49e6326b12d059a6db574
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-08-01 13:46:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.forEach(subArr => {
+      if (str[i] === subArr[0] && str[i + 1] === subArr[1]) {
+        str = str.slice(0, i) + str.slice(i + 2, str.length);
+        i -= 2;
+      }
+    });
+  }
+  return (str.length === 0);
 }
